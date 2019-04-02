d65e854351ac7516917f247786420c61e28aa12f
MokeevDaniil <daniilmokeev13@gmail.com>
2019-04-02 22:22:03 +0300

    lab01_1
    первая лаба (надо доделать вывод)

---
+++ lab01/defs.py
@@ -0,0 +1,134 @@
+﻿def read_points(file_name):
+    curr_x = 0; curr_y = 0;
+    x= [];y=[];
+    try:
+        with open(file_name, 'r') as f:
+            buff = f.readlines();
+            for i in range(len(buff)):
+                    try:
+                        curr_x, curr_y = map(float, buff[i].split());
+                        x.append(curr_x);
+                        y.append(curr_y);
+                    except ValueError:
+                        print('Could not read all the given points');
+    except:
+        return [], []
+
+    return x,y
+
+def triangle_exists(x1, y1, x2, y2, x3, y3):
+    if (y2 - y1)*(x3 - x1) != (y3 - y1)*(x2 - x1):
+        return True
+    else:
+        return False
+
+def find_centre_of_mediane_cross(x1_1, y1_1, x1_2, y1_2, x2_1, y2_1, x2_2, y2_2):
+    #x1  точки первого отрезка, х2 - второго
+    A1 = y1_1 - y1_2
+    B1 = x1_2 - x1_1
+    C1 = x1_1*y1_2 - x1_2*y1_1
+    A2 = y2_1 - y2_2
+    B2 = x2_2 - x2_1
+    C2 = x2_1*y2_2 - x2_2*y2_1
+
+    if B1*A2 - B2*A1 != 0:
+        #Px, Py - точка пересечения
+        try:
+            Py = (C2*A1 - C1*A2) / (B1*A2 - B2*A1)
+            Px = (-C1 - B1*Py) / A1
+        except ZeroDivisionError:
+            #print ("!");
+            pass
+    try:
+        #plt.scatter(Px, Py, color = 'blue');
+        return Px, Py
+    except:
+        #plt.scatter(x1_1, y1_1, color = 'red');
+        #plt.scatter(x1_2, y1_2, color = 'red');
+        return 1, 1
+        
+
+def is_in_triangle(x1, y1, x2, y2, x3, y3, x0, y0):
+    k1 = (x1-x0)*(y2-y1)-(x2-x1)*(y1-y0)
+    k2 = (x2-x0)*(y3-y2)-(x3-x2)*(y2-y0)
+    k3 = (x3-x0)*(y1-y3)-(x1-x3)*(y3-y0)
+    if (k1 > 0 and k2 > 0 and k3 > 0 ) \
+       or (k1 < 0 and k2 < 0 and k3 < 0):
+        return 1
+    else:
+        return 0
+    
+def prosses(x, y):
+    max_diff = 0;
+    max_point = 0;
+    min_point = 0;
+    points_in_triangle = [0]*6
+    result_x = [0] * 3 #масс для вершин конечного треуг
+    result_y = [0] * 3
+    med_1 = [0]*2; med_2 = [0]*2; med_3 = [0]*2 #вершины для медиан [x1, y1]
+    result = []
+    for i in range(len(x)):
+        for j in range(i+1, len(x)):
+            for k in range(j+1, len(x)):
+                #перебор всех точек
+                if triangle_exists(x[i], y[i], x[j], y[j], x[k], y[k]):
+                    #если треугольник существует находим середины отрезков
+#                   x_m_ij - координаты середины стороны соедин i и j вершины
+                    x_m_ij = (x[i]+x[j]) / 2;
+                    y_m_ij = (y[i]+y[j]) / 2;
+
+                    x_m_ik = (x[i]+x[k]) / 2;
+                    y_m_ik = (y[i]+y[k]) / 2;
+
+                    x_m_jk = (x[j]+x[k]) / 2;
+                    y_m_jk = (y[j]+y[k]) / 2;
+
+                    #Px, Py - точка пересечения медиан
+                    Px, Py = find_centre_of_mediane_cross(\
+                        x_m_ij, y_m_ij, x[k], y[k], x_m_ik, y_m_ik, x[j], y[j]);
+
+
+                    #перебор всех точек попадают ли они в каждый из 6-ти треугольников
+                    for t in range(len(x)):
+                        if is_in_triangle(x[i], y[i], Px, Py, x[j], y[j], x[t], y[t]):
+                            points_in_triangle[0] += 1;
+
+                        if is_in_triangle(x[i], y[i], Px, Py, x_m_ik, y_m_ik, x[t], y[t]):
+                            points_in_triangle[1] += 1;
+
+                        if is_in_triangle(x[k], y[k], Px, Py, x_m_ik, y_m_ik, x[t], y[t]):
+                            points_in_triangle[2] += 1;
+
+                        if is_in_triangle(x[k], y[k], Px, Py, x_m_jk, y_m_jk, x[t], y[t]):
+                            points_in_triangle[3] += 1;
+
+                        if is_in_triangle(x[j], y[j], Px, Py, x_m_jk, y_m_jk, x[t], y[t]):
+                            points_in_triangle[4] += 1;
+
+                        if is_in_triangle(x[j], y[j], Px, Py, x_m_ij, y_m_ij, x[t], y[t]):
+                            points_in_triangle[5] += 1;
+
+                    if (max(points_in_triangle) - min(points_in_triangle)) > max_diff:
+                        max_diff = max(points_in_triangle) - min(points_in_triangle)
+                        result_x[0], result_y[0] = x[i], y[i]
+                        result_x[1], result_y[1] = x[j], y[j]
+                        result_x[2], result_y[2] = x[k], y[k]
+
+                        med_1[0], med_1[1] = x_m_jk, y_m_jk
+                        med_2[0], med_2[1] = x_m_ik, y_m_ik
+                        med_3[0], med_3[1] = x_m_ij, y_m_ij
+
+                    #print(points_in_triangle)
+                    
+                    for r in range(len(points_in_triangle)):
+                        points_in_triangle[r] = 0
+                        
+    result.append(result_x)
+    result.append(result_y)
+    result.append(med_1)
+    result.append(med_2)
+    result.append(med_3)
+    
+    return result
+
+    
---
+++ lab01/lab01_class.py
@@ -0,0 +1,232 @@
+from math import *
+from defs import *
+from tkinter import *
+from tkinter import messagebox
+
+
+class triangle():
+    def __init__(self):
+        self.x =[]
+        self.y = []
+        self.result = []
+
+        self.canvas_width = 700
+        self.canvas_height = 700
+        self.real_width = 20
+        self.real_width = 20
+
+        self.len_of_points = 0
+
+        self.coordinates = []
+
+    def print_points(self):
+        n = 2
+        points_label_text.set('')
+        self.len_of_points_label = 0
+        i = 0
+        self.coord = []
+        while i < len(self.x):
+            k = self.canvas_width/self.real_width #на сколько надо сместиться на одно ед.деление
+            #если выходим за пределы, то считаем новые пределы, все удаляем
+            #и строим все точки по новой
+            if abs(self.x[i]) >= self.real_width//2  + 1 \
+               or abs(self.y[i]) >= self.real_width//2 + 1:
+                if abs(self.x[i]) >= self.real_width//2:
+                    self.real_width = 2*abs(self.x[i]) + abs(self.x[i])//10 
+                    self.real_width = 2*abs(self.x[i]) + abs(self.x[i])//10
+                    points_label_text.set('')
+                    len_of_points_label = 0
+                elif abs(y[i]) > self.real_width//2:
+                    self.real_width = 2*abs(y[i]) + abs(y[i])//10
+                    self.real_width = 2*abs(y[i]) + abs(y[i])//10
+                    points_label_text.set('')
+                    len_of_points_label = 0
+                c.delete('all')
+                self.draw_coord()
+                i = 0
+                self.coord = []
+            k = self.canvas_width/(self.real_width)
+                
+            #рисуем точки, n - что-то типо радиуса
+            p = c.create_oval(k*self.x[i] + self.canvas_width//2 - n,\
+                          k *(self.real_width/2 - self.y[i])+ n,\
+                          k*(self.x[i])+ self.canvas_width//2 + n,\
+                          k*(self.real_width/2 - self.y[i])- n,\
+                          fill = 'green')
+            coord = [p, self.x[i], self.y[i]]
+            self.coordinates.append(coord)
+        
+    #попытка печатать в лейбл
+            if self.len_of_points_label < 21:
+                s = points_label_text.get()
+                s+=str(self.x[i])
+                s+=' '
+                s+=str(self.y[i])
+                s+='\n'
+                points_label_text.set(s)
+                self.len_of_points_label += 1
+                s = ''
+            i+=1
+
+    def print_result_triangle(self, result):
+    ##  в result
+    ##  0 - иксы
+    ##  1 - игрики
+    ##  2 - медиана против первой вершины
+    ##  3 - медиана против второй
+    ##  4 - медиана против четвертой
+        
+        k = self.canvas_width/self.real_width
+        #добавляем в конец первую точку чтобы треуг замкнулся
+        result_x = self.result[0]
+        result_y = self.result[1]
+        result_x.append(result_x[0])
+        result_y.append(result_y[0])
+        #print('int canv\n',result)
+        for i in range(len(result_x)-1):
+            c.create_line((k*result_x[i] + self.canvas_width//2,\
+                          k *(self.real_width - result_y[i]) - self.canvas_height//2,\
+                          k*(result_x[i+1])+ self.canvas_width//2,\
+                          k*(self.real_width - result_y[i+1])- self.canvas_height//2))
+        #три медианы пунктиром
+        for i in range(3):
+            c.create_line((k*result_x[i] + self.canvas_width//2,\
+                              k *(self.real_width - result_y[i]) - self.canvas_height//2,\
+                              k*(result[i+2][0])+ self.canvas_width//2,\
+                              k*(self.real_width - result[i+2][1])- self.canvas_height//2), dash = (4,2))
+
+    def input_point(self):
+        c.delete('all')
+        self.draw_coord()
+        self.print_points()
+            
+        if len(self.x) >= 4:
+            self.result = prosses(self.x, self.y)
+            self.print_result_triangle(self.result)
+
+    def draw_coord(self):
+        #рисуем координаты
+        #можно подзапариться над делениями
+        #они рисуются прямо перед мейнлупом
+        c.create_line(0, self.canvas_height//2, self.canvas_width, self.canvas_height//2)
+        c.create_line(self.canvas_width//2, 0, self.canvas_width//2, self.canvas_height)
+
+
+    def add_point(self, x_new, y_new):
+        self.x.append(x_new)
+        self.y.append(y_new)
+
+def clear_press():
+    c.delete('all')
+    draw_coord()
+    x = [];
+    y = [];
+    result = [];
+    points.real_width = 20;
+    points.real_width = 20;
+    points.len_of_points_label = 0
+    points_label_text = ''
+    points.draw_coords()
+
+def enter_file_press():
+    file_name = file_entry.get()
+    if file_name == '':
+        messagebox.showerror("Ошибка ввода", "Не стоит оставлять\nэто поле пустым")
+        return 0
+            #если сначала были введены какие-то точки, а потом читается файл
+            #читаем в новый массик, потом добавляем к уже имеющимся
+    x_new, y_new = read_points(file_name)
+    if(x_new == [] or y_new == []):
+
+        messagebox.showerror("Ошибка ввода",\
+                                     "Такого файла нет.\nНе получилось считать точки")
+        return 0 
+    for j in range(len(x_new)):
+        points.add_point(x_new[j], y_new[j])
+    points.input_point()
+
+def input_point_press():
+    try:
+        x0 = int(x_entry.get())
+        y0 = int(y_entry.get())
+    except:
+        messagebox.showerror("Ошибка ввода",\
+                             "Точка должна иметь\nцелочисленные координаты")
+    points.add_point(x0, y0)
+    points.input_point()
+    x_entry.delete(0, 'end')
+    y_entry.delete(0, 'end')
+    x_entry.focus()
+
+def draw_coordinates(event, a, b):
+    c.bind("<Motion>", lambda event, a = points.canvas_width,\
+           b = points.canvas_height: draw_coordinates(event, a, b))
+
+    x = (event.x - points.canvas_width)/2*2*points.real_width/points.canvas_height + points.real_width/2
+    y = (event.y - points.canvas_width)/2*2*points.real_width/points.canvas_height * (-1) - points.real_width/2
+    label_coords['text'] = '{:.4}, {:.4}'.format(x, y)
+
+
+def leave_canvas(event):
+    label_coords['text'] = '(-/-)'
+    
+    
+root = Tk()
+root.title('Компьютерная графика. Лабораторная работа №1')
+points = triangle()
+c = Canvas(root,\
+           width = points.canvas_width,\
+           height = points.canvas_height,\
+           bg = 'white')
+c.grid(row = 0, column = 0, rowspan = 30)
+
+#ввод из файла
+Label(text = "Ввести точки из файла").grid(row = 2, column = 2, columnspan = 4)
+file_entry = Entry(width = 20)
+file_entry.grid(row = 3, column = 2, columnspan = 4)
+
+file_insert = Button(text = "Enter", width = 10, height = 2,\
+                     command = lambda : enter_file_press())
+file_insert.grid(row = 4, column = 2, columnspan = 4)
+
+#ввод для отдельных точек
+Label(text = "x: ").grid(row = 7, column = 2)
+x_entry = Entry(width = 10)
+x_entry.grid(row = 7, column = 3)
+
+Label(text = "y: ").grid(row = 7, column = 4)
+y_entry = Entry(width = 10)
+y_entry.grid(row = 7, column = 5, columnspan = 1)
+
+point_enter = Button(text = "Ввести точку", width = 10, height = 2,\
+                     command = lambda : input_point_press())
+point_enter.grid(row = 9, column = 2, columnspan = 4)
+
+#Лейбл для уже введеных точек
+Label(text = "Введеные точки:").grid(row = 10, column = 2, columnspan = 4)
+points_label_text = StringVar()
+points_label_text.set('')
+entered_points = Label(textvariable = points_label_text)
+entered_points.grid(row = 11, column = 2, columnspan = 4, rowspan = 20)
+len_of_points_label = 0
+
+#Кнопка очистить канвасик и все засетить на дефолт
+clear_all = Button(text = "Очистить", width = 15, height = 2,\
+                     command = lambda : clear_press())
+clear_all.grid(row = 28, column = 2, columnspan = 4)
+
+#меню бар
+menubar = Menu(root)
+menubar.add_command(label="Задание",\
+                    command = lambda: messagebox.showinfo("Задание",\
+                                           "На плоскости дано множество точек.\nНайти такой треугольник с вершинами в этих точках,\nу которого разность максимального и минимального количества точек,\nпопавших в каждый из 6-ти треугольников,\nобразованных пересечением медиан, максимальна"))
+
+label_coords = Label(text = '(-, -)')
+label_coords.grid(row = 8, column = 1, columnspan = 9)
+c.bind("<Motion>", lambda event, a = points.canvas_width, b = points.canvas_height: draw_coordinates(event, a, b))
+c.bind("<Leave>", leave_canvas)
+
+root.config(menu=menubar)
+points.draw_coord()
+root.mainloop();
+
